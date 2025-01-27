dbe892a990e9c8807c0ba37b619c8144710143a1
EC2 Default User <ec2-user@ip-172-31-30-178.ec2.internal>
2025-01-27 05:43:10 +0000

    fixed the path problem in duckdns

--- Assignment2/duckdns/duck.sh
+++ Assignment2/duckdns/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=bsingh55&token=f2278848-31cf-4c58-bb1b-af65fa693295&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=bsingh55&token=f2278848-31cf-4c58-bb1b-af65fa693295&ip=" | curl -k -o /home/ec2-user/ser321-spring25-A-bsingh55/Assignment2/duckdns/duck.log -K -
